c41950f93ce0f3125c5437e36a0e2c984673aec8
xiongkai <xiongkai01@wondersgroup.com>
2019-04-25 10:42:03 +0800

    发成注册登陆发表功能

--- app.js
+++ app.js
@@ -3,15 +3,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+var flash = require('connect-flash');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+var settings = require('./settings');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(flash());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,16 +25,29 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// handle session
+app.use(session({
+  secret: settings.cookieSecret,
+  key: settings.db,
+  cookie: { maxAge: 1000 * 60 * 60 * 24 * 30 },
+  store: new MongoStore({
+    url:'mongodb://localhost/noblog'
+  }),
+  resave:false,
+  saveUninitialized:true
+}))
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
---
+++ settings.js
@@ -0,0 +1,6 @@
+module.exports={
+    cookieSecret:'noblog',
+    db:'noblog',
+    host:'localhost',
+    port:27017
+};
