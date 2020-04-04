2e8c00bdd12ccd2d9d7a5b67f32accc00cce44f6
xiongkai <xiongkai01@wondersgroup.com>
2020-04-04 20:03:49 +0800

    11

--- models/user.js
+++ models/user.js
@@ -1,5 +1,17 @@
-var mongodb = require('./db')
-var crypto = require('crypto')
+var crypto = require('crypto');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/noblog');
+
+var userSchema = new mongoose.Schema({
+  name: String,
+  password: String,
+  email: String,
+  head: String
+}, {
+    collection: 'users'
+  })
+
+var userModel = mongoose.model('User', userSchema);
 
 function User(user) {
   this.name = user.name;
@@ -7,10 +19,8 @@ function User(user) {
   this.email = user.email;
 }
 
-module.exports = User;
-
 //存储用户信息
-User.prototype.save = function(callback) {
+User.prototype.save = function (callback) {
   var md5 = crypto.createHash('md5'),
     email_MD5 = md5.update(this.email.toLowerCase()).digest('hex'),
     head = "http://www.gravatar.com/avatar/" + email_MD5 + "?s=48";
@@ -22,54 +32,25 @@ User.prototype.save = function(callback) {
     email: this.email,
     head: head
   };
-  //打开数据库
-  mongodb.open(function(err, db) {
+
+  var newUser = new userModel(user);
+
+  newUser.save(function (err, user) {
     if (err) {
-      return callback(err); //错误，返回 err 信息
+      return callback(err);
     }
-    //读取 users 集合
-    db.collection('users', function(err, collection) {
-      if (err) {
-        mongodb.close();
-        return callback(err); //错误，返回 err 信息
-      }
-      //将用户数据插入 users 集合
-      collection.insert(user, {
-        safe: true
-      }, function(err, user) {
-        mongodb.close();
-        if (err) {
-          return callback(err); //错误，返回 err 信息
-        }
-        callback(null, user[0]); //成功！err 为 null，并返回存储后的用户文档
-      });
-    });
+    callback(null, user);
   });
 };
 
 //读取用户信息
-User.get = function(name, callback) {
-  //打开数据库
-  mongodb.open(function(err, db) {
+User.get = function (name, callback) {
+  userModel.findOne({ name: name }, function (err, user) {
     if (err) {
-      return callback(err); //错误，返回 err 信息
+      return callback(err);
     }
-    //读取 users 集合
-    db.collection('users', function(err, collection) {
-      if (err) {
-        mongodb.close();
-        return callback(err); //错误，返回 err 信息
-      }
-      //查找用户名（name键）值为 name 一个文档
-      collection.findOne({
-        name: name
-      }, function(err, user) {
-        mongodb.close();
-        if (err) {
-          return callback(err); //失败！返回 err 信息
-        }
-        callback(null, user); //成功！返回查询的用户信息
-      });
-    });
-  });
+    callback(null, user);
+  })
 };
+
+module.exports = User;
